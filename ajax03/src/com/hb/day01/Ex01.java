0912edaca0f3d1409da883783a3c569e077c6d27
hb06class <hb06class@gmail.com>
2017-11-03 11:16:11 +0900

    jq ajax

---
+++ ajax03/src/com/hb/day01/Ex01.java
@@ -0,0 +1,34 @@
+package com.hb.day01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex01 extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>Á¦¸ñ:"+req.getParameter("sub")+"</h1>");
+		out.print("<p>name:"+req.getParameter("name")+"</p>");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
